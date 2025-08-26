f8fc40b198048d3d09bd21d0fb4169a6cd0de80a
PH🔥 <pablohfogo220@gmail.com>
2025-08-25 22:48:23 -0300

    Sistema de Login e Registre-se funcionando

--- bancodedados/bnc-dd.php
+++ bancodedados/bnc-dd.php
@@ -2,7 +2,7 @@
 $localhost= "localhost";
 $username= "root";
 $password= "";
-$database= "cadastrados";
+$database= "usuarios";
 
 $conn= mysqli_connect($localhost, $username, $password, $database);
 
--- bancodedados/credentials.php
+++ bancodedados/credentials.php
@@ -2,9 +2,36 @@
 
 // print_r($_REQUEST);
 
-if (isset($_POST['submit'])) 
+if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
 {
- header('Location: ../paginas/afterlogin.php');
+    include_once('../bancodedados/bnc-dd.php');
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // print_r('Email: ' . $email);
+    // print_r('<br>');
+    // print_r('Senha: ' . $senha);
+
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
+
+    $result = $conn->query($sql);
+
+    // print_r($sql);
+    // exit;
+
+    if (mysqli_num_rows($result) < 1) {
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        header('Location: ../paginas/login.php');
+        exit();
+    } else {
+        $_SESSION['email'] = $email;
+        $_SESSION['senha'] = $senha;
+        header('Location: ../paginas/afterlogin.php');
+        exit();
+    }
+
 }
 else
 {
